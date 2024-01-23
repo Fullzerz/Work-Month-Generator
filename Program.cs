37c9143974239f830e8cf76cd9c822802d2f508f
Daniele <bonadonnadaniele@gmail.com>
2024-01-23 12:41:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Work_Month_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
